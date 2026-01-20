5716b41ed4b54517a6fc5b97574e1ef7c1392fd2
jpaudel8 <jpaudel8@gmail.com>
2026-01-20 21:19:52 +0545

    iam fixed 1

--- EKS-TF/iam-role.tf
+++ EKS-TF/iam-role.tf
@@ -1,31 +1,4 @@
-resource "aws_iam_role" "EKSClusterRole" {
-  name = "EKSClusterRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role" "NodeGroupRole" {
-  name = "EKSNodeGroupRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+# Fetch the existing LabRole provided by your environment
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
 }
